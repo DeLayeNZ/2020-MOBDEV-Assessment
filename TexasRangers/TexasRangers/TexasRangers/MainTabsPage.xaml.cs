ab231f676447d544f635d7602f96dd13476d7507
Gab <gabdelahaye@gmail.com>
2020-11-25 18:16:48 +1300

    FIRST UPLOAD

---
+++ TexasRangers/TexasRangers/TexasRangers/MainTabsPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TexasRangers
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainTabsPage : TabbedPage
+    {
+        public MainTabsPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void FoodMenu_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new FoodMenuPage());
+        }
+
+        private async void DrinksMenu_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new DrinksMenuPage());
+        }
+    }
+}
